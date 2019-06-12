bf2b8887587cec7ad99949036c36eba098be0df8
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-06-12 12:11:49 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      data: null
+    }
+  }
+  
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.version }))
+      .catch(err => console.log(err));
+  }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async() => {
+    const response = await fetch('https://tranquil-caverns-41069.herokuapp.com/version');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body) 
+    }
+    return body;
+  };
+
+  render() {
+      //"proxy": "http://localhost:5000/"
+      //"proxy": "https://tranquil-caverns-41069.herokuapp.com/"
+
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <p>{this.state.data}</p>
+        </header>
+      
+        <p className="App-intro"></p>
+      </div>
+    );
+  }
+}
+
+export default App;
