f28a2bc86a8969c3e8b51f03154061f8ac3eecc4
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-06-12 16:14:10 -0400

    test

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <p>{versionText} hello</p>
+          <p>{versionText}</p>
         </header>
       
         <p className="App-intro"></p>
