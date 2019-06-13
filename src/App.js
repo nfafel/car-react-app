9e4f1b4fbbc625894934f19e6cde1e6c60c8b51a
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-06-13 14:23:03 -0400

    fixing new row with post

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header" style={{"height":"50%"}}>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">WelcReact</h1>
           <p>{versionText}</p>
         </header>
 
